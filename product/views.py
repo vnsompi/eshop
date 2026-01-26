f62fad34f703a6149d90725518426dca65f8efd2
vainqueur <vnsompi@gmail.com>
2026-01-26 10:36:39 +0100

    added filtering product , pagination , count product functionality

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 """this is when we are going to put all logic of product models"""
 
 from django.shortcuts import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,13 +14,28 @@ from .filters import ProductFilter
 @api_view(['GET'])
 def get_products(request):
 
+    #####################################################################"
     #"""for filtering products"""
     filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
     #"""used filter """
+    #####################################################################"
+    #add count
+    count = filterset.qs.count()
+    ####################################"""
+     #add pagination
+    resPerPage = 1
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    #####################################################################"
 
     #products = Product.objects.all()
-    serializer = ProductSerializer(filterset.qs, many=True)
-    return Response({'products': serializer.data})
+    serializer = ProductSerializer(queryset, many=True)
+    return Response({
+        "count":count,
+        "resPerPage":resPerPage,
+        'products': serializer.data
+    })
 
 """ Single product """
 @api_view(['GET'])
